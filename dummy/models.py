3a53eb55082750ebbf99e7581c1c241b773ab9f3
musa <musa.hstu11@gmail.com>
2024-02-21 22:54:59 +0600

    created vote sqlalchemy model

--- dummy/models.py
+++ dummy/models.py
@@ -22,3 +22,8 @@ class User(Base):
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"))
+
+class Vote(Base): 
+    __tablename__ = "vote"
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True, nullable=False)
